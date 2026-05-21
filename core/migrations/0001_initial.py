3adc34357c49d35c51053ed93684d4e4351a2580
Alex Kaul <bluebirb9@gmail.com>
2026-05-20 20:44:10 -0400

    Fixing

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -4,19 +4,22 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Feature',
+            name="Feature",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('names', models.CharField(max_length=100)),
-                ('colors', models.CharField(max_length=500)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+                    ),
+                ),
+                ("names", models.CharField(max_length=100)),
+                ("colors", models.CharField(max_length=500)),
             ],
         ),
     ]
--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 # Create your models here.
 from django.db import models
 
+
 class Feature(models.Model):
     names = models.CharField(max_length=100)
     colors = models.CharField(max_length=500)
