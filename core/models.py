83807ec8a016642b4065c4aaeef694b4b4872da3
Alex Kaul <bluebirb9@gmail.com>
2026-05-20 20:41:24 -0400

    Designing homepage

--- core/models.py
+++ core/models.py
@@ -1,1 +1,6 @@
 # Create your models here.
+from django.db import models
+
+class Feature(models.Model):
+    names = models.CharField(max_length=100)
+    colors = models.CharField(max_length=500)
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h1>SAT Grinder</h1>")
+    return render(request, "index.html")
