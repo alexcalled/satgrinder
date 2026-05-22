b97ce4083d2289eb4b2846ac4cd6cc463db30318
Alex Kaul <bluebirb9@gmail.com>
2026-05-21 21:43:28 -0400

    Updated homepage

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("/about", views.about, name="about")
-    ]
+urlpatterns = [path("", views.index, name="index"), path("about", views.about, name="about")]
