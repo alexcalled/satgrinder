bca885d46dcebe666e017fda043bd43dcceee54e
Alex Kaul <bluebirb9@gmail.com>
2026-05-31 18:52:30 -0400

    Added grind selection page

--- config/settings.py
+++ config/settings.py
@@ -125,5 +125,5 @@ LOGOUT_REDIRECT_URL = "landing"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-# Create your views here.
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.http import Http404, HttpResponse
 from django.shortcuts import redirect, render
 
 
@@ -9,6 +11,61 @@ def landing(request):
     return render(request, "landing.html")
 
 
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "registration/signup.html", {"form": form})
+
+
 @login_required
 def home(request):
     return render(request, "home.html")
+
+
+@login_required
+def leaderboard(request):
+    return render(request, "leaderboard.html")
+
+
+@login_required
+def grind(request):
+    return render(request, "grind.html")
+
+
+def grind_modal(request, mode):
+    mode_labels = {
+        "reading": "Reading",
+        "writing": "Writing",
+        "math": "Math",
+    }
+
+    if mode not in mode_labels:
+        raise Http404("Invalid grind mode.")
+
+    return render(
+        request,
+        "partials/grind_modal.html",
+        {
+            "mode": mode,
+            "mode_label": mode_labels[mode],
+        },
+    )
+
+
+def grind_modal_close(request):
+    return HttpResponse("")
+
+
+def catchall_view(request, path):
+    return HttpResponse("<h1>404</h1>")
